1ea7d0cce7dbfa41da1b0a0968cddfe5fa762505
TiagoSanti <tiagosanti2009@gmail.com>
2022-03-23 08:13:08 -0400

    Base csharp structure ported from ultra-face-recognition repository

---
+++ CS.Main/FaceDetector.cs
@@ -0,0 +1,69 @@
+﻿using OpenCvSharp;
+using System.Drawing;
+using UltraFaceDotNet;
+
+namespace DeepFace
+{
+    public class FaceDetector
+    {
+        public UltraFace? ultraFace;
+        public string binPath = Helpers.GetProjectPath()+@"\ncnn\data\version-RFB\RFB-320.bin";
+        public string paramPath = Helpers.GetProjectPath()+@".\ncnn\data\version-RFB\RFB-320.param";
+        public UltraFaceParameter? parameter;
+
+        public FaceDetector()
+        {
+            parameter = new UltraFaceParameter
+            {
+                BinFilePath = binPath,
+                ParamFilePath = paramPath,
+                InputWidth = 320,
+                InputLength = 240,
+                NumThread = 1,
+                ScoreThreshold = 0.7f
+            };
+
+            ultraFace = UltraFace.Create(parameter);
+        }
+
+        public FaceInfo[] DetectFacesMat(Mat mat)
+        {
+            using var inMat = NcnnDotNet.Mat.FromPixels(mat.Data, NcnnDotNet.PixelType.Bgr2Rgb, mat.Cols, mat.Rows);
+            
+            return ultraFace.Detect(inMat).ToArray();
+        }
+
+        public FaceInfo[] DetectFacesImagePath(string imagePath)
+        {
+            using var frame = Cv2.ImRead(imagePath);
+            using var inMat = NcnnDotNet.Mat.FromPixels(frame.Data, NcnnDotNet.PixelType.Bgr2Rgb, frame.Cols, frame.Rows);
+
+            return ultraFace.Detect(inMat).ToArray();
+        }
+
+        public void EnchanceDatabaseImages()
+        {
+            string projectDir = Helpers.GetProjectPath();
+            string imageDatabaseDir = projectDir+@"\database\images\";
+
+            string[] peopleImageDBDir = Directory.GetDirectories(imageDatabaseDir);
+            foreach (string personImageDBDir in peopleImageDBDir)
+            {
+                string[] personImagesPath = Directory.GetFiles(personImageDBDir);
+                foreach (string personImagePath in personImagesPath)
+                {
+                    if (!personImagePath.Contains("_cropped"))
+                    {
+                        FaceInfo[] faceInfos = DetectFacesImagePath(personImagePath);
+                        if (faceInfos != null)
+                        {
+                            FaceInfo faceInfo = faceInfos[0];
+                            using Bitmap bitmap = Helpers.CropImageFromPath(personImagePath, faceInfo);
+                            Helpers.OverwriteImage(bitmap, personImagePath);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- CS.Main/Program.cs
+++ CS.Main/Program.cs
@@ -1,1 +1,76 @@
-﻿
+﻿using OpenCvSharp;
+using UltraFaceDotNet;
+
+namespace DeepFace
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            FaceDetector detector = new();
+            Helpers.CreateTempDir();
+            Helpers.ClearTemp();
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            DatabaseEncodings(detector);
+            watch.Stop();
+            Console.WriteLine("Time to run DatabaseEncodings(): " + watch.Elapsed.TotalSeconds.ToString());
+
+            RunRealTimeRecognizer();
+        }
+
+        public static void RunRealTimeRecognizer()
+        {
+            FaceDetector detector = new();
+            FaceRecognizer faceRecognizer = new();
+            Camera capture = new();
+            capture.StartCamera();
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            while (Window.WaitKey(10) != 27)
+            {
+                using Mat? mat = capture.GetFrame();
+                if (mat != null)
+                {
+                    FaceInfo[] faceInfos = detector.DetectFacesMat(mat);
+                    if (faceInfos.Length > 0)
+                    {
+                        IEnumerable<FaceInfo> validFaceInfos = from faceInfo in faceInfos
+                                                               where faceInfo.Score > 0.9
+                                                               select faceInfo;
+                        Drawers.DrawFacesRects(mat, faceInfos);
+
+                        if (watch.Elapsed.TotalSeconds > 3)
+                        {
+                            if (faceRecognizer.GetPyTaskStatus() == TaskStatus.Created)
+                            {
+                                faceRecognizer.RunPyTask();
+                            }
+                            else if (faceRecognizer.GetPyTaskStatus() == TaskStatus.Running)
+                            {
+                                if (!Helpers.ThereIsTempFiles())
+                                {
+                                    List<Mat> croppedMats = Helpers.BitmapsToMats(Helpers.CropImageFromMat(mat, faceInfos));
+                                    Helpers.SaveTempImage(croppedMats);
+                                }
+                            }
+
+                            watch.Restart();
+                        }
+                    }
+
+                    capture.ShowImage(mat);
+                }
+            }
+            watch.Stop();
+            Helpers.ClearTemp();
+            Camera.Close();
+        }
+
+        public static void DatabaseEncodings(FaceDetector detector)
+        {
+            detector.EnchanceDatabaseImages();
+            Encoder.EncodeDatabase();
+        }
+    }
+}
