f060a2b7f72adb5d7c952467973fa529738e8f05
TiagoSanti <tiagosanti2009@gmail.com>
2022-03-23 08:36:10 -0400

    Directories parameters ajusted

--- CS.Main/Encoder.cs
+++ CS.Main/Encoder.cs
@@ -5,8 +5,8 @@
         public static void EncodeDatabase()
         {
             string projectPath = Helpers.GetProjectPath();
-            string scriptPath = projectPath + "\\scripts\\database_encoder.py";
-            string databasePath = projectPath + "\\database";
+            string scriptPath = projectPath + "\\PyScripts\\database_encoder.py";
+            string databasePath = projectPath + "\\Database";
             string[] args = {scriptPath, databasePath};
 
             PythonScript script = new(args);
