34d262864a4054d80c81ff55151b64c5f1ea616b
TiagoSanti <tiagosanti2009@gmail.com>
2022-03-25 11:39:56 -0400

    Result saving implemented

--- PyScripts/verification.py
+++ PyScripts/verification.py
@@ -1,16 +1,48 @@
 import sys
 import os
+from os import path
+from datetime import datetime
 from deepface import DeepFace
+from deepface.commons import functions
 import numpy as np
 import pandas as pd
+import cv2
 import time
+from DeepFaceAPI import *
+
+
+def save_result(image_path, result_distance, result_score, result_knn):
+    # (width, height)
+
+    result_distance_pred = result_distance[0][0]
+    result_score_pred = result_score[0][0]
+    result_knn_pred = result_knn[0][0]
+
+    img = cv2.imread(image_path)
+    cv2.resize(img, (100, 120))
+
+    result_dir = 'C:\\Dev\\Github\\TiagoSanti\\deep-face\\Results\\'
+    try:
+        os.mkdir(result_dir)
+    except FileExistsError:
+        pass
+
+    datet = datetime.now().strftime('%S_%f')
+    results = f'd.{result_distance_pred}-s.{result_score_pred}-k.{result_knn_pred}_'
+    file_name = result_dir + results + datet + '.jpg'
+    cv2.imwrite(file_name, img)
+    return file_name
+
 
 try:
     database_path = sys.argv[1]
     temp_path = sys.argv[2]
-except:
+except IndexError:
     database_path = r'C:\Dev\Github\TiagoSanti\deep-face\Database'
     temp_path = r'C:\Dev\Github\TiagoSanti\deep-face\Temp'
+
+model_name = 'ArcFace'
+metric = 'cosine'
 
 while True:
     start = time.time()
@@ -18,18 +50,18 @@ while True:
     temp_files = os.listdir(temp_path)
 
     if len(temp_files) > 0:
-        result = None
-        print('begin verification')
         for temp_file in temp_files:
             temp_file_path = temp_path + '\\' + temp_file
-            result = DeepFace.find(temp_file_path,
+
+            temp_img = cv2.imread(temp_file_path)
+
+            result = DeepFace.find(temp_img,
                                    db_path=database_path,
-                                   model_name='ArcFace',
-                                   distance_metric='cosine',
+                                   model_name=model_name,
+                                   distance_metric=metric,
                                    detector_backend='mtcnn',
                                    enforce_detection=False)
 
-        if result is not None:
             # files paths
             paths = result['identity'].values.tolist()
             identity = []
@@ -55,12 +87,16 @@ while True:
 
             result['score'] = score
 
+            print(result)
+            print()
+            sys.stdout.flush()
+
             distance_mean = []
             score_mean = []
 
             # mean distances and scores
             for id_name in unique_id:
-                id_distances = result[result['identity'] == id_name]['ArcFace_cosine'].values
+                id_distances = result[result['identity'] == id_name][f'{model_name}_{metric}'].values
                 id_scores = result[result['identity'] == id_name]['score'].values
 
                 id_distances_lenght = len(id_distances)
@@ -97,20 +133,29 @@ while True:
 
             print(df)
             sys.stdout.flush()
-            # Printing results
 
-            print('\nResult by distance mean:')
-            print(df.sort_values(by=['distance_mean'])[:1])
+            result_distance = df.sort_values(by=['distance_mean'])[:1]
+            result_score = df.sort_values(by=['score_mean'], ascending=False)[:1]
+            result_knn = df.sort_values(by=['knn'], ascending=False)[:1]
+
+            result_file_name = save_result(temp_file_path,
+                                           result_distance.values.tolist(),
+                                           result_score.values.tolist(),
+                                           result_knn.values.tolist())
+
+            print('\nRESULT FILE:', result_file_name.split('\\')[-1])
+            print('Result by distance mean:')
+            print(result_distance)
 
             print('\nResult by score mean:')
-            print(df.sort_values(by=['score_mean'], ascending=False)[:1])
+            print(result_score)
 
             print('\nResult by k_neighbors:')
-            print(df.sort_values(by=['knn'], ascending=False)[:1])
+            print(result_knn)
 
             print(f'\n{(time.time() - start):.2f} seconds to verify\n')
             sys.stdout.flush()
 
             os.remove(temp_file_path)
 
-    time.sleep(10)
+    time.sleep(5)
